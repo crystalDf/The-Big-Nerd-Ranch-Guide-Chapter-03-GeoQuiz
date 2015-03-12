bb4a08d7b39b3edc14904d32a657d68c25008b1a
crystalDf <chendong333@gmail.com>
2015-03-12 17:51:06 +0800

    Chapter 03\GeoQuiz finished

---
+++ app/src/main/java/com/star/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package com.star.geoquiz;
+
+/**
+ * Created by chendong on 2014/10/30.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int mQuestion, boolean mTrueQuestion) {
+        this.mQuestion = mQuestion;
+        this.mTrueQuestion = mTrueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
